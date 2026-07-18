c67f9aae8bb6f739732a65c2e1727992eb0e8d2f
moraesvalterdev2026-byte <moraesvalter.dev2026@gmail.com>
2026-07-18 17:45:48 -0300

    fix(jest): simplifica configuracao de testes para evitar falhas no CI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 // jest.config.js
 module.exports = {
+  testEnvironment: "node",
   // Arquivo que será executado antes de todos os testes
   setupFiles: ['<rootDir>/setup.js'],
   // Diretório onde os testes estão localizados
