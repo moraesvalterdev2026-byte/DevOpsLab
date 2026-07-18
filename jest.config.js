d5ac746e6e69d805bc677d045a5c25189f42889f
moraesvalterdev2026-byte <moraesvalter.dev2026@gmail.com>
2026-07-18 17:41:19 -0300

    fix(jest): corrige diretório raiz dos testes para o pipeline

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   // Arquivo que será executado antes de todos os testes
   setupFiles: ['<rootDir>/setup.js'],
   // Diretório onde os testes estão localizados
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>'],
 };
