bb8bd5c2aa8875d37599d12497b51aebcac50b23
moraesvalterdev2026-byte <moraesvalter.dev2026@gmail.com>
2026-07-18 17:38:11 -0300

    fix(jest): corrige caminho do arquivo setup.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // jest.config.js
 module.exports = {
   // Arquivo que será executado antes de todos os testes
-  setupFiles: ['<rootDir>/tests/setup.js'],
+  setupFiles: ['<rootDir>/setup.js'],
   // Diretório onde os testes estão localizados
   roots: ['<rootDir>/tests'],
 };
